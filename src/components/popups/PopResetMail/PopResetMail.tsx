70101e208f95ee4350f039b7ccf29889624e55fe
Yaroslav Sakhno <yaroslav.saxno@yandex.ru>
2024-06-26 22:36:07 +0300

    Добавлена валидация полей ввода

--- src/components/popups/PopResetMail/PopResetMail.tsx
+++ src/components/popups/PopResetMail/PopResetMail.tsx
@@ -2,7 +2,7 @@ export default function PopResetMail() {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="popup-container">
-        <div className="flex items-center justify-center mb-[48px]">
+        <div className="logo-container">
           <img src="../public/logo.png" alt="logo" />
         </div>
         <p className="text-center whitespace-pre-line text-lg mr-[28px] ml-[30px]">
