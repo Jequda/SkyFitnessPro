b28142cd2db21a1fac3ca3bddc6c462bf048ffc6
EkaterinaMikhalko <mikhalkoea@gmail.com>
2024-07-14 13:16:50 +0300

    Прописана логика в header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,104 +1,130 @@
-import classNames from "classnames";
 import { useState } from "react";
 import PopLogin from "../popups/PopLogin/PopLogin";
 import PopUserMenu from "../popups/PopUserMenu/PopUserMenu";
+import PopSignin from "../popups/PopSignin/PopSignin";
 
 export default function Header() {
-  const [isOpened, setIsOpened] = useState(false);
-  function togglePopup() {
-    setIsOpened((prev) => !prev);
+  //API не подключено, для смены кнопки "Войти" на инфо о пользователе нужно поменять user true/false
+  const user = true;
+
+  const [isUserMenuOpened, setIsUserMenuOpened] = useState(false);
+  const [isLoginOpened, setIsLoginOpened] = useState(false);
+  const [isSigninOpened, setIsSigninOpened] = useState(false);
+
+  function openPopLogin() {
+    setIsLoginOpened((prev) => !prev);
+    if (isSigninOpened) {
+      openPopSignin();
+    }
   }
-  
+
+  function openPopSignin() {
+    setIsSigninOpened((prev) => !prev);
+    if (isLoginOpened) {
+      openPopLogin();
+    }
+  }
+
+  function toggleUserMenuPopup() {
+    setIsUserMenuOpened((prev) => !prev);
+  }
+
   return (
     <>
-      <div className="flex flex-row">
-        <div className="mt-[50px] ml-[140px] ">
-          <img
-            className=""
-            src="/logo.png"
-            alt="course_picture"
-            width="220px"
-            height="35px"
-          />
-          <p className="mt-[15px] w-[327px] text-lg text-[#7d7d7d]">
-            Онлайн-тренировки для занятий дома
-          </p>
-        </div>
-        <div className="ml-[730px] mt-[50px]">
-          <button
-            onClick={togglePopup}
-            className="btn-green w-[103px] h-[52px] text-2xl py-2 px-4 font-semibold text-center"
-          >
-            Войти
-          </button>
-          {isOpened && (
-            <div
-              className={classNames(
-                "fixed inset-0 bg-black bg-opacity-50 z-10 flex items-center justify-center",
-                isOpened ? "" : "hidden"
-              )}
-              onClick={() => togglePopup()}
-            >
-              <div onClick={(e) => e.stopPropagation()}>
-                <PopLogin />
+      {user ? (
+        <div className="flex flex-row">
+          <div className="mt-[50px] ml-[140px] ">
+            <img
+              className=""
+              src="/logo.png"
+              alt="course_picture"
+              width="220px"
+              height="35px"
+            />
+            <p className="mt-[15px] w-[327px] text-lg text-[#7d7d7d]">
+              Онлайн-тренировки для занятий дома
+            </p>
+          </div>
+          <div className="ml-[685px] mt-[50px] ">
+            <div className="">
+              <div
+                onClick={toggleUserMenuPopup}
+                className=" w-[168px] h-[50px] flex flex-row"
+              >
+                <img
+                  className=""
+                  src="/Profile.png"
+                  alt="profile_picture"
+                  width="50px"
+                  height="50px"
+                />
+                <p className="text-lg/[26px] flex items-center pl-[15px] pr-[15px]">
+                  Сергей
+                </p>
+                <img
+                  className=""
+                  src="/Rectangle 3765.svg"
+                  alt=""
+                  width="8px"
+                  height="8px"
+                />
               </div>
             </div>
-          )}
+            {isUserMenuOpened && (
+              <div className="" onClick={() => toggleUserMenuPopup()}>
+                <div onClick={(e) => e.stopPropagation()}>
+                  <PopUserMenu />
+                </div>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="flex flex-row">
+          {(isLoginOpened || isSigninOpened) && (
+            <div
+              onClick={() => {
+                setIsLoginOpened(false), setIsSigninOpened(false);
+              }}
+              className="fixed inset-0 bg-black bg-opacity-50 z-[3]"
+            ></div>
+          )}
+          <div className="mt-[50px] ml-[140px] ">
+            <img
+              className=""
+              src="/logo.png"
+              alt="course_picture"
+              width="220px"
+              height="35px"
+            />
+            <p className="mt-[15px] w-[327px] text-lg text-[#7d7d7d]">
+              Онлайн-тренировки для занятий дома
+            </p>
+          </div>
+          <div className="ml-[730px] mt-[50px]">
+            <button
+              onClick={openPopLogin}
+              className="btn-green w-[103px] h-[52px] text-2xl py-2 px-4 font-semibold text-center"
+            >
+              Войти
+            </button>
+          </div>
+          <div className="absolute z-[4] bg-white rounded-[30px] mt-[267px] ml-[540px]">
+            {isLoginOpened && (
+              <PopLogin
+                transitionFromMainPage={true}
+                openPopSignin={openPopSignin}
+              />
+            )}
+            {isSigninOpened && (
+              <PopSignin
+                transitionFromMainPage={true}
+                openPopLogin={openPopLogin}
+              />
+            )}
+          </div>
+        </div>
+      )}
     </>
   );
 }
-
-// Если пользователь авторизован
-
-//   return (
-//     <>
-//       <div className="flex flex-row">
-//         <div className="mt-[50px] ml-[140px] ">
-//           <img
-//             className=""
-//             src="/logo.png"
-//             alt="course_picture"
-//             width="220px"
-//             height="35px"
-//           />
-//           <p className="mt-[15px] w-[327px] text-lg text-[#7d7d7d]">
-//             Онлайн-тренировки для занятий дома
-//           </p>
-//         </div>
-//         <div className="ml-[685px] mt-[50px] ">
-//           <div className="">
-//           <div onClick={togglePopup} className=" w-[168px] h-[50px] flex flex-row">
-//             <img
-//               className=""
-//               src="/Profile.png"
-//               alt="profile_picture"
-//               width="50px"
-//               height="50px"
-//             />
-//             <p className="text-lg/[26px] flex items-center pl-[15px] pr-[15px]">Сергей</p>
-//             <img
-//               className=""
-//               src="/Rectangle 3765.svg"
-//               alt=""
-//               width="8px"
-//               height="8px"
-//             />
-//             </div>
-//           </div>
-//           {isOpened && (
-//             <div
-//               className=""
-//               onClick={() => togglePopup()}
-//             >
-//               <div onClick={(e) => e.stopPropagation()}>
-//                 <PopUserMenu/>
-//               </div>
-//             </div>
-//           )}
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
--- src/components/popups/PopUserMenu/PopUserMenu.tsx
+++ src/components/popups/PopUserMenu/PopUserMenu.tsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import { appRoutes } from "../../../route/appRoutes";
+
 export default function PopUserMenu() {
   return (
     <div className="fixed inset-0 z-50 mt-[120px]">
@@ -6,8 +9,12 @@ export default function PopUserMenu() {
             <p className="text-center text-xl">Сергей</p>
             <p className="text-center text-xl text-[#999999] mt-[10px]">sergey.petrov96@mail.ru</p>
             <div className="pt-[34px]">
+                <Link to={appRoutes.PROFILE}>
                 <button className="btn-green w-[206px] h-[52px]">Мой профиль</button>
+                </Link>
+                <Link to={appRoutes.LOGIN}>
                 <button className="btn-white w-[206px] h-[52px] mt-[10px] ">Выйти</button>
+                </Link>
             </div>
         </div>
     </div>
