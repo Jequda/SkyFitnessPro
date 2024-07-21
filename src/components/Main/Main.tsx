0335ba3974c252d9c3d9f825c6359da73cccf1b1
твое имя < >
2024-07-21 23:27:21 +0400

    1

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -18,11 +18,11 @@ export default function Main() {
 
   useEffect(() => {
     getCoursesList();
-  }, [cards, userId]);
+  }, []);
 
   useEffect(() => {
     getNotAddedCardsList();
-  }, [userId, cards, notAddedCards]);
+  }, []);
 
   const [isOpenedPopLogin, setIsOpenedPopLogin] = useState<boolean>(false);
   const openPopLogin = () => {
