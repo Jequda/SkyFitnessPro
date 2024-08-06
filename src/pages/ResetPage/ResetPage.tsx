a7576851ee85d28bb4d450a4a871b6c53e4af31c
Yaroslav Sakhno <yaroslav.saxno@yandex.ru>
2024-08-06 04:55:43 +0300

    Пофиксил ошибки перед деплоем

--- src/pages/ResetPage/ResetPage.tsx
+++ src/pages/ResetPage/ResetPage.tsx
@@ -1,5 +1,11 @@
 import PopReset from "../../components/popups/PopReset/PopReset";
 
 export default function SigninPage() {
-  return <PopReset />;
+  return (
+    <PopReset
+      onClose={function (): void {
+        throw new Error("Function not implemented.");
+      }}
+    />
+  );
 }
